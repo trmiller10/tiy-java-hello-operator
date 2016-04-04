6138e934604a98c358353a04df8dadafab0a36ef
Doug Hughes <doug@theironyard.com>
2016-04-04 16:27:46 -0400

    Added instructions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# tiy-java-hello-operator
-Java and Closure assignment: Hello Operator!
+## Fork the Repository
+
+[I've created a repository for this project on Github.](https://github.com/TIYRaleigh/tiy-java-hello-operator) [Fork this repository](https://help.github.com/articles/fork-a-repo/) to create a version for yourself.
+
+## Instructions
+
+1. Clone your forked copy of the repository.
+2. Fix follow the instructions in HelloOperator.java
---
+++ src/HelloOperator.java
@@ -0,0 +1,60 @@
+/**
+ * Follow the instructions in the comments below.
+ * Only add one line per comment. The output from this program should be:
+ *
+ *      134.4
+ *      true
+ *      125.19999999999999 = 125.2? false
+ *      425.0
+ *      425.0
+ *      false
+ *      true
+ *      false
+ *      false
+ */
+public class HelloOperator {
+
+    public static void main(String[] args){
+
+        // Create a variable named "result". Set it to 504.
+
+        // Divide "result" by 10 and then add 84 to it.
+
+        // output "result" to the console
+
+        // create a new variable named "expected". Set this to 134.4.
+
+        // create a variable named "matches". Compare "result" and "expected". If they're the same then set "matches" to true.
+
+        // output whether or not the two variables match
+
+        // reset "result" to 125.1
+
+        // reset "expected" to 125.3
+
+        // add 0.1 to "result"
+
+        // subtract 0.1 from "expected"
+
+        // Compare "result" and "expected". If they're the same then set "matches" to true.
+
+        // output the value of "result", the value of "expected", and the whether or not they are equal. (EG: result = expected? matches)
+
+        // Start with 56. Multiply it by the sum of 50 and 26. Subtract 6. Divide this by the remainder left over when you divide 723 by 23. Set "result" to the resulting value.
+
+        // output "result"
+
+        // Add 1203 and 72. Divide the total by 3 and set "expected" to the result.
+
+        // output "expected"
+
+        // Output true if "result" is greater than "expected"
+
+        // Output true if "result" is less than "expected"
+
+        // Output true if "result" is equal to "expected"
+
+        // Output false if either "result" or "expected" are not equal to 425
+
+    }
+}
